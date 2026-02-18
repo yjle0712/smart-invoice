785f60a60d11998d972aae40720573e8c22fea7f
Youssef Jlile <yjlile@gmail.com>
2026-02-18 01:29:04 +0000

    fix: add pdfjs type declarations

---
+++ src/types/pdfjs-dist.d.ts
@@ -0,0 +1,4 @@
+declare module "pdfjs-dist/legacy/build/pdf";
+declare module "pdfjs-dist/legacy/build/pdf.worker";
+declare module "pdfjs-dist/build/pdf";
+declare module "pdfjs-dist/build/pdf.worker";
